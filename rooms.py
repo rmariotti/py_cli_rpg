ad37419659b084a06e60d3f27e0144e3e937505d
Rodolfo Mariotti <rodo.mariotti@gmail.com>
2013-12-19 16:45:30 +0100

    added method create_subdoor

--- rooms.py
+++ rooms.py
@@ -3,8 +3,15 @@
 
 
 class Room:
-    def __init__(self, items=[], doors=[], entities=[], description=""):
+    def __init__(self, items=[], doors=[], entities=[], description="",
+                 is_locked=False):
         self.items = items
         self.doors = doors
         self.entities = entities
         self.description = description
+
+    def create_subdoor(self, items=[], doors=[] entities=[], description="",
+                       is_locked=False):
+        doors.append(self)
+        a = self.__init__(items, doors, entities, description is_locked)
+        return a
